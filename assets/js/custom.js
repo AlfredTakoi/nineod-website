9990e4b995dccc5c1698ccfd3d05cb9d8106c8d2
AlfredTakoi <alfredtakoi@gmail.com>
2022-09-13 17:09:39 +0700

    update navbar and pagination section, class active when scroll and clicked

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,6 @@
 // $(document).ready(function(){
 //   $(this).scrollTop(0);
 // });
-
-$(".menu-toggle").click(function () {
-  $(".bar1").toggleClass("change");
-  $(".bar2").toggleClass("change");
-  $(".bar3").toggleClass("change");
-});
-
 $(".hamburger").click(function () {
   $(".bar").toggleClass("is-active");
   $(".hamburger").toggleClass("is-active");
@@ -26,16 +19,40 @@ $("#service-link").click(function () {
 $(".page-scroll").on("click", function (e) {
   const element = $(this).attr("href");
   const goalElement = $(element);
+  $("html, body").animate({
+    scrollTop: goalElement.offset().top - 70,
+  });
   $(".wrapper-menu").toggleClass("open");
   $(".mobile-nav").toggleClass("is-active");
-  $(window).scrollTop(goalElement.offset().top - 90);
   e.preventDefault();
 });
 
-$('.arrow-prev').mouseenter(function(){
-  console.log("is entered")
-})
+$(window).scroll(function () {
+  $(".section-row").each(function () {
+    sectionRowTop = $(this).offset().top - 100;
+    if ($(window).scrollTop() > sectionRowTop) {
+      $(".section-row").removeClass("active");
+      ActiveId = $(this).addClass("active").attr("id");
+    }
+  });
 
+  $(".item-nav").each(function () {
+    thisChildrenHref = $(this).children("a").attr("href");
+    if (thisChildrenHref === "#" + ActiveId) {
+      $(".item-nav").removeClass("active");
+      $(this).addClass("active");
+    }
+  });
+
+  $(".page-item").each(function () {
+    thisChildren = $(this).children("a");
+    thisChildrenHref = $(this).children("a").attr("href");
+    if (thisChildrenHref === "#" + ActiveId) {
+      $(".page-item a").removeClass("active");
+      $(thisChildren).addClass("active");
+    }
+  });
+});
 
 $(".partners-body").slick({
   infinite: true,
