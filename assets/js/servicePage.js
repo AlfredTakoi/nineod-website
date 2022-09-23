bb45fd8f99e64f8bebf3e4f9a47a432f449b0a97
AlfredTakoi <alfredtakoi@gmail.com>
2022-09-23 15:48:14 +0700

    adding feature search in accordion

--- assets/js/servicePage.js
+++ assets/js/servicePage.js
@@ -1,6 +1,39 @@
-$(document).ready(function(){
-    $('.search-icon').click(function(){
-        $(this).addClass("float-start")
-        $(this).removeClass("float-end")
-    })
-})
+$(document).ready(function () {
+  $(".search-icon").click(function () {
+    $(this).toggleClass("active");
+    $(".input-search").toggleClass("active");
+    $(".btn-close").toggleClass("active");
+  });
+  $(".btn-close").click(function () {
+    $(this).toggleClass("active");
+    $(".input-search").toggleClass("active");
+    $(".search-icon").toggleClass("active");
+  });
+
+  var showError = true;
+  $(".input-search").on("keyup", function () {
+    const value = $(this).val().toLowerCase();
+    if (value === "") {
+      $(".message-empty").hide();
+    }
+    $(".accordion-item").each(function () {
+      const htxt = $(this).text().toString().toLowerCase();
+      const accIndex = $(this).index();
+      if (htxt.indexOf(value) > -1) {
+        $(this).show();
+        $("#accordion-item-" + accIndex)
+          .attr("filter-key", value)
+          .show();
+        showError = false;
+      } else if ($(".accordion-item:visible").length === 0) {
+        $(".message-empty").show();
+      } else {
+        $(this).hide();
+        $(".message-empty").hide();
+      }
+    });
+    if (showError) {
+      $(".message-empty").hide();
+    }
+  });
+});
